dac8c994085002f42746d11572dbf5b3a9f0701a
blkZ <a1135065316@gmail.com>
2020-11-26 15:16:06 +0800

    111

---
+++ components/custom-video-player/custom-video-player.vue
@@ -0,0 +1,33 @@
+<template>
+	<video
+	id="custom_video"
+	:src='videoUrl'
+	>
+		
+	</video>
+</template>
+
+<script>
+	export default {
+		props: {
+			videoUrl: {
+				type: String,
+				required: true,
+			}, // 视频地址
+		},
+		data() {
+			return {
+				videoContext: null, // 视频上下文
+			};
+		},
+		created() {
+			// 创建视频上下文
+			this.videoContext = uni.createVideoContext('custom_video', this)
+			this.videoContext.requestFullScreen()
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,34 +1,23 @@
 <template>
 	<view>
-		<image src="../../static/logo.png" mode="" class="plhImg"></image>
-		
-		<img-tailor class="imgTailor" img='../../static/logo.png' :boxHeight='400' :boxWidth='300' ref='imgTailor'></img-tailor>
-		<button @tap="finish">完成</button>
+		<customVideoPlayer :videoUrl='videoUrl'></customVideoPlayer>
 	</view>
 </template>
 
 <script>
-	import imgTailor from '@/components/imgTailor/imgTailor.vue'
+	import customVideoPlayer from '@/components/custom-video-player/custom-video-player.vue'
 	export default {
 		data() {
 			return {
+				videoUrl: 'https://cdn.lelepen.com/uploads/video/catelog/2020/75156439524bb29333490de410b8ec98_PD.mp4'
 			}
 		},
 		components: {
-			imgTailor
-		},
-		methods: {
-			finish() {
-				this.$refs.imgTailor.cutImg()
-			}
+			customVideoPlayer
 		}
 	}
 </script>
 
-<style lang="scss" scoped>
-	.imgTailor {
-		width: 300px;
-		height: 400px;
-		margin-left: 30px;
-	}
+<style scoped>
+
 </style>
